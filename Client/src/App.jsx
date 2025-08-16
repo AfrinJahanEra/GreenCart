982fa6566a479bd61213e188e9ce64f9466bae0a
era97 <afrinjahanera03@gmail.com>
2025-08-16 15:50:15 +0600

    pages routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import PlantCollection from './pages/PlantCollection'
+import PlantDetails from './pages/PlantDetails'
+import NotFound from './pages/NotFound'
+import Order from './pages/Order'
+import OrderConfirmation from './pages/OrderConfirmation'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/plants" element={<PlantCollection />} />
+          <Route path="/plants/:category" element={<PlantCollection />} />
+          <Route path="/plant/:id" element={<PlantDetails />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/order-confirmation" element={<OrderConfirmation />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
